629776b08862e389b2a56dc60c67f000a121969e
maruhachi <konchan1129@gmail.com>
2017-06-19 01:35:47 +0900

    練習 6/19

--- js/index.js
+++ js/index.js
@@ -1,33 +1,54 @@
-(function() {
-    // 上記のようにすることで名前空間を限定できる
-  
-    'use strict';
- 
- 
-    // ---------------------------------------------------------------------------
-    // 初期化関数定義 (ページの DOM ツリー構築完了後に行う初期処理)
-    // ---------------------------------------------------------------------------
- 
-    /**
-     * 初期化関数。
-     * - イベントハンドラを設定する。
-     * - アラートを表示する。
-     */
-    // $(function init() {
- 
-      var app = new Vue({
-        el: '#app',
-        data: {
-          message : 'fix, Messages'
-        }
-      })
-    // });
- 
- 
-    // ---------------------------------------------------------------------------
-    // 関数定義 (イベントハンドラ以外)
-    // ---------------------------------------------------------------------------
- 
-    // 何かあれば。
- 
-  }());
+
+'use strict';
+
+/* はじめに https://jp.vuejs.org/v2/guide/#%E5%AE%A3%E8%A8%80%E7%9A%84%E3%83%AC%E3%83%B3%E3%83%80%E3%83%AA%E3%83%B3%E3%82%B0 */
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello, Vue.js!'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data:{ message: 'You loaded this page on ' + new Date()}
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen : true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      {text: 'Learn Javascript'},
+      {text: 'Learn Vue'},
+      {text: 'Build something awesome'}
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message : 'Hello vue.js!'
+  },
+  methods: {
+    reverseMessage: function (){
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message : 'Hello , Vue!'
+  }
+})
+
+/* コンポーネントによる構成 https://jp.vuejs.org/v2/guide/#%E3%82%B3%E3%83%B3%E3%83%9D%E3%83%BC%E3%83%8D%E3%83%B3%E3%83%88%E3%81%AB%E3%82%88%E3%82%8B%E6%A7%8B%E6%88%90 */
